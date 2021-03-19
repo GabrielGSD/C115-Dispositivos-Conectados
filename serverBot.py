35270147a2d3ef1b7d3ae59b86f62266c7a294ee
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-03-19 17:18:03 -0300

    final

--- serverBot.py
+++ serverBot.py
@@ -2,50 +2,52 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_host = socket.gethostname()
-udp_port = 12000
+udp_port = 8080
 sock.bind((udp_host, udp_port))
 
 i = 0
-
+s = 0
 while True:
     
     data, addr = sock.recvfrom(1024)
     Data = data.decode()
-    
     print(Data)
 
+    if Data == "00":
+        respo = "\n Olá,\n Clique 1 para crédito \n Clique 2 para débito\n Clique 3 para sair"
+        sock.sendto(respo.encode('utf-8'), addr)
+        i = 0
+        Data = "0"
+
     if Data == "1" and i == 0:
-        respo = "\nPara extrato das faturas tecle 1 \n Para valor fatura tecle 2 \n Para encerrar tecle 3"
+        respo = "\n Para extrato das faturas tecle 1 \n Para valor fatura tecle 2 \n Para encerrar tecle 3"
         sock.sendto(respo.encode('utf-8'), addr)
         i = 1
-        Data = "0"
-    else:
-        if Data == "2" and i == 0:
-            respo = "\nPara extrato tecle 1 \n Para transferencia tecle 2 \n Para encerrar tecle 3"
-            sock.sendto(respo.encode('utf-8'), addr)
-            i = 2
-            Data = "0"
+        Data = ""
+    elif Data == "2" and i == 0:
+        respo = "\nPara extrato tecle 1 \nPara transferencia tecle 2 \nPara encerrar tecle 3"
+        sock.sendto(respo.encode('utf-8'), addr)
+        i = 2
+        Data = ""
 
     if Data == "1" and i == 1:
-        respo = "\nSeu saldo é de R$1000,00"
+        respo = "\nSeu saldo é de R$857,23,00"
         sock.sendto(respo.encode('utf-8'), addr)
         i = 0
-        Data = "0"
-    else:
-        if Data == "2" and i == 1:
-            respo = "Sua fatura é de R$ 600,57"
-            sock.sendto(respo.encode('utf-8'), addr)
-            i = 0
-            Data = "0"
-    
-    if Data == "1" and i == 2:
-        respo = "\nSeu saldo é de R$1000,00"
+        Data = ""
+    elif Data == "1" and i == 2:
+        respo = "\nSeu saldo é de R$700,99"
         sock.sendto(respo.encode('utf-8'), addr)
         i = 0
-        Data = "0"
-    else:
-        if Data == "2" and i == 2:
-            respo = "Não é possível tranferir no momento. Tente mais tarde!"
-            sock.sendto(respo.encode('utf-8'), addr)
-            i = 0
-            Data = "0"
+        Data = ""
+
+    if Data == "2" and i == 1:
+        respo = "Sua fatura é de R$ 600,57"
+        sock.sendto(respo.encode('utf-8'), addr)
+        i = 0
+        Data = ""
+    elif Data == "2" and i == 2:
+        respo = "Não é possível tranferir no momento. Tente mais tarde!"
+        sock.sendto(respo.encode('utf-8'), addr)
+        i = 0
+        Data = ""
--- udpClient.py
+++ udpClient.py
@@ -1,22 +1,32 @@
 import socket
-
+i = 0
 while True:
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_host = socket.gethostname()
-    udp_port = 12000
+    udp_port = 8080
 
-    msg = input("\nBom dia,\nclique 1 para crédito \nClique 2 para débito\n ")
-    
+    if i == 0:
+        msga = "00"
+        sock.sendto(msga.encode(), (udp_host, udp_port))
+        resp = sock.recv(1024)
+        print(resp.decode('utf-8'))
+        msg = input(" Opção 1: ")
+        i = 1
+
     if msg == "3":
-        print("ERRO!! SAINDO...")
+        print("SAINDO...")
         sock.close()
         break
+
+    elif msg != "1" and msg != "2" and msg != "3":
+        print("ERRO!!! Código Inválido!")
+
     else:
         sock.sendto(msg.encode(), (udp_host, udp_port))
         resp = sock.recv(1024)
         print(resp.decode('utf-8'))
-        msg2 = input("\nOpção 2: ")
+        msg2 = input(" Opção 2: ")
         sock.sendto(msg2.encode(), (udp_host, udp_port))
         resp = sock.recv(1024)
         print(resp.decode('utf-8'))
+        i = 0
